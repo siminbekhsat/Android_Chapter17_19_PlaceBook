9326883d06c4954e9f906649fcb50b72907d0d91
Simin Bekhsat <56419294+siminbekhsat@users.noreply.github.com>
2022-03-26 22:50:29 -0230

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
@@ -0,0 +1,58 @@
+package com.raywenderlich.placebook.adapter
+
+import android.app.Activity
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.raywenderlich.placebook.R
+import com.raywenderlich.placebook.ui.MapsActivity
+import com.raywenderlich.placebook.viewmodel.MapsViewModel
+
+class BookmarkInfoWindowAdapter(val context: Activity) :
+    GoogleMap.InfoWindowAdapter {
+
+
+  private val contents: View
+
+
+  init {
+    contents = context.layoutInflater.inflate(
+        R.layout.content_bookmark_info, null)
+  }
+
+  override fun getInfoWindow(marker: Marker): View? {
+    return null
+  }
+
+
+
+  override fun getInfoContents(marker: Marker): View? {
+    val titleView = contents.findViewById<TextView>(R.id.title)
+    titleView.text = marker.title ?: ""
+
+
+    val phoneView = contents.findViewById<TextView>(R.id.phone)
+    phoneView.text = marker.snippet ?: ""
+
+
+    val imageView = contents.findViewById<ImageView>(R.id.photo)
+    when (marker.tag) {
+
+      is MapsActivity.PlaceInfo -> {
+        imageView.setImageBitmap(
+            (marker.tag as MapsActivity.PlaceInfo).image)
+      }
+
+      is MapsViewModel.BookmarkView -> {
+        var bookMarkview = marker.tag as
+            MapsViewModel.BookmarkView
+        imageView.setImageBitmap(bookMarkview.getImage(context))
+      }
+    }
+    return contents
+  }
+
+
+}
---
+++ app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkListAdapter.kt
@@ -0,0 +1,68 @@
+package com.raywenderlich.placebook.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.raywenderlich.placebook.R
+import com.raywenderlich.placebook.databinding.BookmarkItemBinding
+import com.raywenderlich.placebook.ui.MapsActivity
+import com.raywenderlich.placebook.viewmodel.MapsViewModel.BookmarkView
+
+class BookmarkListAdapter(
+
+    private var bookmarkData: List<BookmarkView>?,
+    private val mapsActivity: MapsActivity) :
+    RecyclerView.Adapter<BookmarkListAdapter.ViewHolder>() {
+
+
+
+  class ViewHolder(val binding: BookmarkItemBinding,
+                   private val mapsActivity: MapsActivity) :
+      RecyclerView.ViewHolder(binding.root) {
+
+    init {
+      binding.root.setOnClickListener {
+        val bookmarkView = itemView.tag as BookmarkView
+        mapsActivity.moveToBookmark(bookmarkView)
+      }
+    }
+
+
+  }
+
+
+
+  fun setBookmarkData(bookmarks: List<BookmarkView>) {
+    this.bookmarkData = bookmarks
+    notifyDataSetChanged()
+  }
+
+
+  override fun onCreateViewHolder(
+      parent: ViewGroup,
+      viewType: Int): ViewHolder {
+    val layoutInflater = LayoutInflater.from(parent.context)
+    val binding = BookmarkItemBinding.inflate(layoutInflater, parent, false)
+    return ViewHolder(binding, mapsActivity)
+  }
+
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    val bookmarkData = bookmarkData ?: return
+    val bookmarkViewData = bookmarkData[position]
+
+
+    holder.binding.root.tag = bookmarkViewData
+    holder.binding.bookmarkData = bookmarkViewData
+    bookmarkViewData.categoryResourceId?.let {
+      holder.binding.bookmarkIcon.setImageResource(it)
+    }
+  }
+
+
+
+  override fun getItemCount(): Int {
+    return bookmarkData?.size ?: 0
+  }
+}
